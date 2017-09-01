ab78a3c63c6b3f64957cd6f1b02d84e9ba43b653
Pandzic Ivan <pandzic.ivan@nsoft.ba>
2017-09-01 13:28:53 +0200

    Initial commit -a

--- src/plugin.js
+++ src/plugin.js
@@ -1,24 +1,32 @@
-import _ from 'lodash';
-
 const plugin = (editor) => {
-  editor.addButton('tinymceGenericPlugin', {
-    text: 'tinymceGenericPlugin',
-    icon: false,
-    onclick: () => {
-      // Open window
-      editor.windowManager.open({
-        title: 'tinymceGenericPlugin plugin',
-        body: [
-          { type: 'textbox', name: 'title' }
-        ],
-        onsubmit(e) {
-          // Insert content when the window form is submitted
-          const kebabbyString = _.kebabCase(e.data.title);
-          editor.insertContent(kebabbyString);
+    editor.addButton('tinymceGenericPlugin', {
+        text: editor.getParam('generic_plugin_data').plugin_text || false,
+        icon: editor.getParam('generic_plugin_data').plugin_icon || false,
+        tooltip: editor.getParam('generic_plugin_data').plugin_tooltip || '',
+        onclick: () => {
+            editor.windowManager.open({
+                title: editor.getParam('generic_plugin_data').plugin_title || 'Generic plugin',
+                body: [
+                    {
+                        type: 'iframe',
+                        id:"tinymce-generic-plugin",
+                        url: editor.getParam('generic_plugin_data').plugin_frame_url,
+                        minWidth: editor.getParam('generic_plugin_data').plugin_width || 800,
+                        minHeight: editor.getParam('generic_plugin_data').plugin_height || 600
+                    }
+                ],
+                width: editor.getParam('generic_plugin_data').plugin_width || 800,
+                height: editor.getParam('generic_plugin_data').plugin_height || 800,
+                buttons: [{
+                    text: editor.getParam('generic_plugin_data').plugin_close_txt || 'Close',
+                    onclick: 'close'
+                }],
+                scrollbars: true
+            }, {
+                customData : editor.getParam('generic_plugin_data')
+            });
         }
-      });
-    }
-  });
+    });
 };
 
 export default plugin;
